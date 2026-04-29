d7ddacb02d97bf5edb8fc7ea6ed9e2146adf1f0b
Caio Rodrigues <caiogbr@edu.unifor.br>
2026-04-29 17:37:53 -0300

    :sparkles:feat: change the design for the filter activity_filter.xml

--- app/src/main/java/com/example/uniforeventos/FilterActivity.kt
+++ app/src/main/java/com/example/uniforeventos/FilterActivity.kt
@@ -1,6 +1,9 @@
 package com.example.uniforeventos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,15 @@ class FilterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<ImageView>(R.id.ivBack).setOnClickListener {
+            finish()
+        }
+
+        findViewById<LinearLayout>(R.id.llViewCalendars).setOnClickListener {
+            val intent = Intent(this, DateTimeFilterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
