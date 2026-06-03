508226f6ec472798bd9c3dd0881144e6e30b9167
Gabriel Neri <gabriel.neri.qa@gmail.com>
2026-06-02 22:16:38 -0300

    fix: adiciona DTOs de emprestimo em network/dto

---
+++ app/src/main/java/com/example/uniforeventos/network/dto/EmprestimoRequestDTO.kt
@@ -0,0 +1,7 @@
+﻿package com.example.uniforeventos.network.dto
+
+data class EmprestimoRequestDTO(
+    val usuarioId: Long,
+    val livroId: Long,
+    val fimEmprestimo: String? = null
+)
---
+++ app/src/main/java/com/example/uniforeventos/network/dto/EstenderEmprestimoDTO.kt
@@ -0,0 +1,5 @@
+﻿package com.example.uniforeventos.network.dto
+
+data class EstenderEmprestimoDTO(
+    val fimEmprestimo: String
+)
